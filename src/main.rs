3e020aed3e47969e60d95c185d27098016ddc7f8
rust <vavruska.pavel@gmail.com>
2022-05-26 22:17:03 +0200

    feat: saving args values in variables

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,14 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+
+    if args.len() < 3 {
+        panic!("Usage: {} [TEXT_TO_SEARCH] [FILE]", &args[0])
+    }
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
 }
