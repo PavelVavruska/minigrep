d9f98859533aa1d62d100f7475bd74cef510b92a
rust <vavruska.pavel@gmail.com>
2022-05-27 16:36:04 +0200

    feat: main function refactor

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
 use std::env;
 use std::fs;
+use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);  // program exited with an error state
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(config.filename)
-    .expect("Something went wrong reading the file");
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
 
-    println!("With text:\n{}", contents);
+        process::exit(1);
+    }
 }
 
 struct Config {
@@ -21,13 +27,21 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("ERROR: Not enough arguments. Usage: {} [TEXT_TO_SEARCH] [FILE]", &args[0])
+            return Err("Not enough arguments.");
         }
         let query = args[1].clone();
         let filename = args[2].clone();
     
-        Config {query, filename}
+        Ok(Config { query, filename })
     }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    println!("With text:\n{}", contents);
+
+    Ok(())
 }
